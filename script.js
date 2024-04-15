6cdad3bffb357e7c46a9b49c1024f928ed9336ef
noahmeridew <74562754+noahmeridew@users.noreply.github.com>
2024-04-15 14:23:42 -0400

    Added scroll effect

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function smoothScroll(target) {
         window.open('path_to_your_pdf_file.pdf', '_blank');
       };
 }
+
+const revealElements = document.querySelectorAll("[data-reveal]")
+
+const scrollReveal = function () {
+    for (let i = 0, len = revealElements.length; i < len; i++) {
+        const isElementsOnScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight;
+
+        if (isElementsOnScreen) {
+            revealElements[i].classList.add("revealed")
+        }
+        else {
+            revealElements[i].classList.remove("revealed");
+        }
+    }
+}
+
+window.addEventListener("scroll", scrollReveal);
+
+window.addEventListener("load", scrollReveal);
